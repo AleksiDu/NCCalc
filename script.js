db67a8eb1cdb5d45f45ba051bc0c6c591d8fe9ce
AleksiDu <aleksi.duluzauri@gmail.com>
2022-05-31 20:56:18 +0400

    feat: Add dropdown button for mm to inch

--- script.js
+++ script.js
@@ -4,19 +4,18 @@
 
 // import Tools from "./Tools";
 
-
-
 const div = document.createElement("div");
 div.classList.add("topnav");
 
 div.innerHTML = `
 <a href=""><button class="cta-button print">Print</button></a>
   <div class="dropdown">
-  <button class="dropbtn" >Dropdown
-    <i class="fa fa-caret-down"></i>
+  <button class="cta-button units" >
+  Units
+  <i class="fa fa-caret-down"></i>
   </button>
   <div class="dropdown-content" id="myDropdown">
-    <a href="#">MM</a>
+    <a href="">MM</a>
     <a href="#">Inch</a>
   </div>
   </div> 
@@ -24,35 +23,18 @@ div.innerHTML = `
 <a href=""><button class="cta-button info">Info</button></a>
 `
 
-
-const dropDown = div.querySelector(".dropdown");
-dropDown.addEventListener("click", () => {
-    button.classList.toggle("show");
-
-    // Close the dropdown if the user clicks outside of it
-    window.onclick = function (e) {
-        if (!e.target.matches('.dropbtn')) {
-            var myDropdown = document.getElementById("myDropdown");
-            if (myDropdown.classList.contains('show')) {
-                myDropdown.classList.remove('show');
-            }
-        }
-    }
-}, false);
-
-
 const button = div.querySelector(".print");
 // Log when the button is clicked in the console.
 button.addEventListener("click", () => {
-    button.classList.toggle("active");
-    //Print a specific div from a page
-    var prtContent = document.getElementsByClassName("result");
-    var WinPrint = window.open('', '', 'left=0,top=0,width=800,height=900,toolbar=0,scrollbars=0,status=0');
-    WinPrint.document.write(prtContent.innerHTML);
-    WinPrint.document.close();
-    WinPrint.focus();
-    WinPrint.print();
-    // WinPrint.close();
+  button.classList.toggle("active");
+  //Print a specific div from a page
+  var prtContent = document.getElementsByClassName("result");
+  var WinPrint = window.open('', '', 'left=0,top=0,width=800,height=900,toolbar=0,scrollbars=0,status=0');
+  WinPrint.document.write(prtContent.innerHTML);
+  WinPrint.document.close();
+  WinPrint.focus();
+  WinPrint.print();
+  WinPrint.close();
 }, false);
 
 
