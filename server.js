b3e0de29c67fea697c57d77eddca208e1d991b8d
AleksiDu <aleksi.duluzauri@gmail.com>
2022-06-16 21:15:16 +0400

    init: Start backend

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile(process.cwd() + '/index.html');
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
+
