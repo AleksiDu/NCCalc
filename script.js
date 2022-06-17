fd765d45e34d364133ef23475a9fadf8d6a38316
AleksiDu <aleksi.duluzauri@gmail.com>
2022-06-17 21:31:13 +0400

    feat: add info modal

--- script.js
+++ script.js
@@ -29,7 +29,18 @@ div.innerHTML = `
     <a href="#" class ="light">Light theme</a>
   </div>
   </div> 
-<a href="#"><button class="cta-button info">Info</button></a>
+<a href="#"><button class="cta-button info" id="myBtn">Info</button></a>
+<!-- The Modal -->
+<div id="myModal" class="modal">
+
+  <!-- Modal content -->
+  <div class="modal-content">
+    <span class="close">&times;</span>
+    <p>Info about application</p>
+  </div>
+
+</div>
+
 <a href=""><button class="cta-button-right table">Table</button></a>
 <a href="#"><button class="cta-button-right drill">Drill</button></a>
 <a href="#"><button class="cta-button-right mill">Mill</button></a>
@@ -118,6 +129,33 @@ light.addEventListener("click", () => {
   rootElement.style.setProperty("--theme-text", lightText);
   rootElement.style.setProperty("--theme-text-menu", lightTextMenu);
 });
+
+/**
+ *  Info
+ */
+
+// Get the modal
+let modal = div.querySelector("#myModal");
+// Get the button that opens the modal
+let btn = div.querySelector("#myBtn");
+// Get the <span> element that closes the modal
+let span = div.querySelector(".close");
+// When the user clicks the button, open the modal 
+btn.addEventListener("click", () => {
+  modal.style.display = "block";
+});
+// When the user clicks on <span> (x), close the modal
+span.addEventListener("click", () => {
+  modal.style.display = "none";
+})
+
+// When the user clicks anywhere outside of the modal, close it
+window.addEventListener("click", (event) => {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+})
+
 
 /**
  * Tool Data
