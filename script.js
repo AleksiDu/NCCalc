3a5988bb65fc4288ef3774a91965113bd258b9a4
AleksiDu <aleksi.duluzauri@gmail.com>
2022-06-18 17:00:53 +0400

    fix: print btn fix

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const div = document.createElement("div");
 div.classList.add("topnav");
 
 div.innerHTML = `
-<a href=""><button class="cta-button print">Print</button></a>
+<a href="#"><button class="cta-button print">Print</button></a>
   <div class="dropdown">
   <button class="cta-button units" >
   Units
@@ -100,8 +100,8 @@ const button = div.querySelector(".print");
 button.addEventListener("click", () => {
   button.classList.toggle("active");
   //Print a specific div from a page
-  var prtContent = document.getElementsByClassName("result");
-  var WinPrint = window.open('', '', 'left=0,top=0,width=800,height=900,toolbar=0,scrollbars=0,status=0');
+  let prtContent = document.getElementsByClassName("result");
+  let WinPrint = window.open('', '', 'left=0,top=0,width=800,height=900,toolbar=0,scrollbars=0,status=0');
   WinPrint.document.write(prtContent.innerHTML);
   WinPrint.document.close();
   WinPrint.focus();
