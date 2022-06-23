386fca5ccff19f6f176599c2a32e05508ce20a5c
AleksiDu <aleksi.duluzauri@gmail.com>
2022-06-23 15:15:24 +0400

    feat: add sql tool_list

--- pg_db/tool_list.sql
+++ pg_db/tool_list.sql
@@ -1,2 +1,13 @@
-CREATE DATABASE NCCalc WITH OWNER = postgres ENCODING = 'UTF8' LC_COLLATE = 'English_United States.1252' LC_CTYPE = 'English_United States.1252' TABLESPACE = pg_default CONNECTION
+CREATE DATABASE "NCCalc" WITH OWNER = postgres ENCODING = 'UTF8' CONNECTION
 LIMIT = -1;
+CREATE SCHEMA tool_list AUTHORIZATION postgres;
+CREATE TABLE tool_list.tool_list (
+    id serial NOT NULL,
+    name character varying(12) NOT NULL,
+    type character varying(12),
+    diameter character varying(12) NOT NULL,
+    speed integer,
+    feed integer,
+    PRIMARY KEY (id)
+);
+ALTER TABLE IF EXISTS tool_list.tool_list OWNER to postgres;
